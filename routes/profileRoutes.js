d5ec983b4d405eb393ff2e9d89994903dd1f5d54
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-08-24 20:31:17 -0500

    final edit: fix routes, remove console logs

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,12 +1,48 @@
 const express = require("express");
 const profileRoutes = express.Router();
 
-profileRoutes.get("/profile/:id", (req, res) => {
-  let requestedUserId = parseInt(req.params.id);
-  //parse the data for this now
-  let userPage = data.users.find(user => user.id === requestedUserId);
+const mongo = require("mongodb");
+const ObjectId = mongo.ObjectID;
+const dbUrl = "mongodb://localhost:27017/userDirectory";
+const MongoClient = mongo.MongoClient;
+let DB;
+let Robots;
 
-  res.render("profile", userPage);
+MongoClient.connect(dbUrl, (err, db) => {
+  if (err) {
+    return console.log("error connecting to the database", err);
+  }
+  DB = db;
+  Robots = db.collection("robots");
+});
+
+profileRoutes.get("/:id", (req, res) => {
+  Robots.findOne({ _id: ObjectId(req.params.id) }, (err, foundRobot) => {
+    if (err) res.status(500).send(err);
+    if (!foundRobot) res.send("No User Found");
+    res.render("profile", { data: foundRobot });
+  });
+});
+
+profileRoutes.get("/:skillName", (req, res) => {
+  Robots.find({ skills: req.params.skillName }).toArray((err, foundRobots) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.render("index", { users: foundRobots });
+  });
+});
+
+profileRoutes.get("/:countryName", (req, res) => {
+  Robots.find({
+    "address.country": req.params.countryName
+  }).toArray((err, foundRobots) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    res.render("index", { users: foundRobots });
+  });
 });
 
 module.exports = profileRoutes;
--- server.js
+++ server.js
@@ -4,105 +4,23 @@ const mustacheExpress = require("mustache-express");
 const port = process.env.PORT || 8000;
 const indexRoutes = require("./routes/indexRoutes");
 const profileRoutes = require("./routes/profileRoutes");
-
+const empRoutes = require("./routes/employmentRoutes");
 const mongo = require("mongodb");
 const MongoClient = mongo.MongoClient;
 const ObjectId = mongo.ObjectID;
 const dbUrl = "mongodb://localhost:27017/userDirectory";
-let DB;
-let Robots;
-
 const app = express();
 
-MongoClient.connect(dbUrl, (err, db) => {
-  if (err) {
-    return console.log("error connecting to the database", err);
-  }
-
-  DB = db;
-  Robots = db.collection("robots");
-});
-
-// This is how you can add from a js object file
-
-// app.get("/addrobots", (req, res) => {
-//   Robots.insertMany(data.users, (err, savedRobots) => {
-//     if (err) {
-//       res.status(500).send(err);
-//     }
-//     res.send(savedRobots);
-//   });
-// });
-
+//templating engine
 app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
 app.set("view engine", "mustache");
-
 app.use(express.static(path.join(__dirname, "public")));
 
 //routes
-// app.use("/", indexRoutes);
-// app.use("/profile", profileRoutes);
-
-app.get("/", (req, res) => {
-  Robots.find({}).toArray((err, foundRobots) => {
-    err
-      ? res.status(500).send(err)
-      : res.render("index", { users: foundRobots });
-  });
-});
-
-app.get("/profile/:id", (req, res) => {
-  Robots.findOne({ _id: ObjectId(req.params.id) }, (err, foundRobot) => {
-    if (err) res.status(500).send(err);
-    if (!foundRobot) res.send("No User Found");
-    res.render("profile", { data: foundRobot });
-  });
-});
-
-app.get("/unemployed", (req, res) => {
-  Robots.find({ job: null }).toArray((err, foundRobots) => {
-    err
-      ? res.status(500).send(err)
-      : res.render("index", { users: foundRobots });
-  });
-});
-
-app.get("/employed", (req, res) => {
-  Robots.find({
-    job: { $not: { $in: [null] } }
-  }).toArray((err, foundRobots) => {
-    err
-      ? res.status(500).send(err)
-      : res.render("index", { users: foundRobots });
-  });
-});
-
-// app.get("/profile/:country", (req, res) => {
-//   Robots.find({});
-// });
-
-app.get("/skills/:skillName", (req, res) => {
-  Robots.find({ skills: req.params.skillName }).toArray((err, foundRobots) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    console.log({ users: foundRobots });
-    res.render("index", { users: foundRobots });
-  });
-});
-
-app.get("/country/:countryName", (req, res) => {
-  Robots.find({
-    "address.country": req.params.countryName
-  }).toArray((err, foundRobots) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-
-    res.render("index", { users: foundRobots });
-  });
-});
+app.use("/", indexRoutes);
+app.use("/profile", profileRoutes);
+app.use("/employment", empRoutes);
 
 app.listen(port, function() {
   console.log(`server is running on port ${port}!`);
